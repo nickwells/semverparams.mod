6576ab0cba7c2059d52db70e87bc2074c6f561b7
Nick Wells <wells.nicholas@gmail.com>
2019-03-10 16:37:44 +0000

    add a semver config file

--- v2/go.mod
+++ v2/go.mod
@@ -2,6 +2,8 @@ module github.com/nickwells/semverparams.mod/v2
 
 require (
 	github.com/nickwells/check.mod v1.1.4
-	github.com/nickwells/param.mod/v2 v2.0.1
+	github.com/nickwells/filecheck.mod v1.0.4
+	github.com/nickwells/param.mod/v2 v2.0.2
 	github.com/nickwells/semver.mod v1.1.2
+	github.com/nickwells/xdg.mod v1.0.1
 )
--- v2/semverparams/params.go
+++ v2/semverparams/params.go
@@ -2,11 +2,15 @@ package semverparams
 
 import (
 	"errors"
+	"path/filepath"
+	"sync"
 
 	"github.com/nickwells/check.mod/check"
+	"github.com/nickwells/filecheck.mod/filecheck"
 	"github.com/nickwells/param.mod/v2/param"
 	"github.com/nickwells/param.mod/v2/param/psetter"
 	"github.com/nickwells/semver.mod/semver"
+	"github.com/nickwells/xdg.mod/xdg"
 )
 
 // SemVer is a semantic version number that will be set by the parameter
@@ -23,6 +27,21 @@ var BuildIDs []string
 
 var semverParam *param.ByName
 
+const semverGroupName = "semver"
+
+var setGroupOnce sync.Once
+
+// addSVGroup will add the semver group to the set of parameter groups
+func addSVGroup(ps *param.PSet) {
+	setGroupOnce.Do(func() {
+		ps.AddGroup(semverGroupName,
+			"common parameters concerned with semantic version numbers")
+		ps.AddGroupConfigFile(semverGroupName,
+			filepath.Join(xdg.ConfigHome(), "semver.config"),
+			filecheck.Optional)
+	})
+}
+
 // SetAttrOnSVStringParam allows you to set any desired attributes on the
 // parameter which is used to supply the semantic version number. a typical
 // use might be to set the param.MustBeSet attribute to ensure that there is
@@ -30,7 +49,7 @@ var semverParam *param.ByName
 func SetAttrOnSVStringParam(attrs param.Attributes) error {
 	if semverParam == nil {
 		return errors.New("the semver parameter has not been created yet. " +
-			" Call semverparams.AddParamVersion before setting the attributes")
+			" Call semverparams.AddSVStringParam before setting the attributes")
 	}
 
 	return param.Attrs(attrs)(semverParam)
@@ -39,9 +58,12 @@ func SetAttrOnSVStringParam(attrs param.Attributes) error {
 // AddSVStringParam will add parameters for setting the semantic version
 // number to the passed PSet
 func AddSVStringParam(ps *param.PSet) error {
+	addSVGroup(ps)
 	semverParam = ps.Add("semver", &SVSetter{Value: &SemVer},
 		"specify the semantic version number to be used",
-		param.AltName("svn"))
+		param.AltName("svn"),
+		param.GroupName(semverGroupName),
+	)
 
 	return nil
 }
@@ -49,6 +71,7 @@ func AddSVStringParam(ps *param.PSet) error {
 // AddIDParams will add parameters for setting the pre-release and build IDs
 // of a semantic version number to the passed PSet
 func AddIDParams(ps *param.PSet) error {
+	addSVGroup(ps)
 	ps.Add("pre-rel-IDs",
 		psetter.StrList{
 			Value:            &PreRelIDs,
@@ -61,6 +84,7 @@ func AddIDParams(ps *param.PSet) error {
 		"specify a non-empty list of pre-release IDs"+
 			" suitable for setting on a semantic version number",
 		param.AltName("prIDs"),
+		param.GroupName(semverGroupName),
 	)
 
 	ps.Add("build-IDs",
@@ -75,6 +99,7 @@ func AddIDParams(ps *param.PSet) error {
 		"specify a non-empty list of build IDs"+
 			" suitable for setting on a semantic version number",
 		param.AltName("bldIDs"),
+		param.GroupName(semverGroupName),
 	)
 
 	return nil
