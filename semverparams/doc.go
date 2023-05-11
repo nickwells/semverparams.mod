5f9ed07ad43757347b8644f3d4c0be849c11c922
Nick Wells <wells.nicholas@gmail.com>
2023-05-11 10:50:15 +0100

    refactor: remove the redundant blank lines

--- semverparams/doc.go
+++ semverparams/doc.go
@@ -1,8 +1,6 @@
 /*
-
 Package semverparams offers various AddParam... funcs for adding useful
 parameters to a parameter set. These parameters are for working with semantic
 version numbers.
-
 */
 package semverparams
